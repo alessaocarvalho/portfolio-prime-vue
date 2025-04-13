f71ff6550581d7825d02ce6eca076cddfcf8fa23
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2025-04-13 15:39:05 -0300

    Alterações para deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-prime-vue/',
   plugins: [
     vue(),
     vueDevTools(),
