3d0a3273931d4db190f5ba77110ac44de8cd7c7c
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2025-04-13 15:48:20 -0300

    Alterações para deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
 import HeroSection from "@/components/HeroSection.vue";
 import AboutSection from "@/components/AboutSection.vue";
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
